3bce2c4e91347b3a22e4672df27e3c3c2c5340d2
Edmon <eduardo.mhdz@gmail.com>
2020-09-18 16:04:23 -0500

    página inicial

---
+++ src/main/java/com/montes/proyectose/web/controller/HostController.java
@@ -0,0 +1,15 @@
+package com.montes.proyectose.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HostController {
+
+    @GetMapping
+    public String gateway() {
+        return "hola pto";
+    }
+}
